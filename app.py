78a73accafe8603f006d5c15bfe43504adf1f6b1
RubelHK99 <83302988+RubelHK99@users.noreply.github.com>
2021-04-28 00:55:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, render_template, request, send_from_directory
 import cv2
 import keras
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
 
 import os
